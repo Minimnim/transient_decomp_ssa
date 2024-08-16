5ce4c1f174119d55f41dfdbca8651c558dcec79f
Minoo Ashoori <78046636+Minimnim@users.noreply.github.com>
2024-08-16 14:59:09 +0100

    Update gen_epoch_window.m

--- utils/gen_epoch_window.m
+++ utils/gen_epoch_window.m
@@ -1,36 +1,3 @@
-%-------------------------------------------------------------------------------
-% gen_epoch_window: calculate overlap size (in samples) and window length for
-% overlap-and-add type analysis
-%
-% Syntax: [L_hop,L_epoch,win_epoch]=gen_epoch_window(L_overlap,L_epoch,win_type,Fs)
-%
-% Inputs: 
-%     L_overlap - precentage overlap 
-%     L_epoch   - epoch size (in seconds)
-%     win_type  - window type, e.g. 'hamm' for Hamming window
-%     Fs        - sampling frequency (in Hz)
-%
-% Outputs: 
-%     L_hop     - hop size (in samples)
-%     L_epoch   - epoch size (in samples)
-%     win_epoch - window, of length L_epoch 
-%
-% Example:
-%     overlap=50; win_length=2; Fs=64
-%
-%     [L_hop,L_epoch,win_epoch]=gen_epoch_window(overlap,win_length,'hamm',Fs);
-%   
-%     fprintf('hop length=%d; epoch length=%d\n',L_hop,L_epoch);
-%     figure(1); clf; 
-%     ttime=(0:(length(win_epoch)-1))./Fs;
-%     plot(ttime,win_epoch);
-
-
-% John M. O' Toole, University College Cork
-% Started: 28-05-2013
-%
-% last update: Time-stamp: <2017-03-14 17:31:08 (otoolej)>
-%-------------------------------------------------------------------------------
 function [L_hop,L_epoch,win_epoch]=gen_epoch_window(L_overlap,L_epoch,win_type,Fs, ...
                                                     GEN_PSD)
 if(nargin<5 || isempty(GEN_PSD)), GEN_PSD=0; end
@@ -39,7 +6,7 @@ if(nargin<5 || isempty(GEN_PSD)), GEN_PSD=0; end
 L_hop=(100-L_overlap)/100;
 
 
-L_epoch=floor( L_epoch*Fs );
+L_epoch=floor( L_epoch * 1/6 );
 
 % check for window type to force constant-overlap add constraint
 % i.e. \sum_m w(n-mR)=1 for all n, where R=overlap size
@@ -89,6 +56,7 @@ else
         win_epoch(end)=win_epoch(end)/2;
     end    
 end
+
 
 
 
